c452fe804c54d6389d9308657b69d26e5d6705d0
Arek T <arkadiusztruszkowski94@gmail.com>
2021-07-11 19:52:54 -0400

    7/11

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -28,8 +28,10 @@ def create_app(test_config=None):
     def hello():
         return "Hello, World!"
 
-    from . import db, auth
+    from . import db, auth, blog
     db.init_app(app)
     app.register_blueprint(auth.bp)
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
 
     return app
